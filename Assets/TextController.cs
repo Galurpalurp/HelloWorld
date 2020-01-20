a289716708d8f00e2f385bb05972c2b071189838
Galurpalurp <59857253+Galurpalurp@users.noreply.github.com>
2020-01-19 22:21:08 -0500

    First Push

---
+++ Assets/TextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public Text newText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            newText.text = "Hello, my name is Nadyah Al-Ghamdi!";
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
